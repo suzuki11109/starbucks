f18f10d84fa253bb3ae55e6a49e3cfa89b513ce0
Suzuki Aki <suzuki11109@gmail.com>
2016-10-14 17:21:22 +0700

    refactor properties of order to pricable items

--- order.go
+++ order.go
@@ -2,33 +2,45 @@ package starbucks
 
 func PlaceOrder(size string, coffeeType string, temperature string) Order {
 	return Order{
-		size:        getSize(size),
-		coffeeType:  getCoffeeType(coffeeType),
-		temperature: createTemperature(temperature),
+		items: []PricableItem{
+			getSize(size),
+			getCoffeeType(coffeeType),
+			createTemperature(temperature),
+		},
 	}
 }
 
 type Order struct {
-	coffeeType  CoffeeType
-	size        Size
-	temperature Temperature
+	items []PricableItem
+}
+
+type PricableItem interface {
+	getPrice() int
 }
 
 func (o Order) getPrice() int {
 	var price int
-	price += o.size.getPrice()
-	price += o.coffeeType.getPrice()
-	price += o.temperature.getPrice()
-
+	for _, v := range o.items {
+		price += v.getPrice()
+	}
 	return price
 }
 
 func (o Order) getCoffee() Coffee {
-	return Coffee{
-		shorts:      o.size.getShorts(),
-		coffeeType:  o.coffeeType.name(),
-		temperature: o.temperature.getTemperature(),
+	coffee := Coffee{}
+
+	for _, v := range o.items {
+		switch v.(type) {
+		case Size:
+			coffee.shorts = v.(Size).getShorts()
+		case CoffeeType:
+			coffee.coffeeType = v.(CoffeeType).name()
+		case Temperature:
+			coffee.temperature = v.(Temperature).getTemperature()
+		}
 	}
+
+	return coffee
 }
 
 type Coffee struct {
